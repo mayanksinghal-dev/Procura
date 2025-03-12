599a8c90a7668e24103ebb41507659b16ae7aa6c
mayanksinghal-dev <mayanksinghal.dev@gmail.com>
2025-03-12 16:31:11 +0530

    Initial commit

---
+++ src/main/java/com/procura/inventory/ProcuraApplication.java
@@ -0,0 +1,13 @@
+package com.procura.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/procura/inventory/ProcuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procura.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
